d8638f4051fc6e4578d11a839de6c40373a4d78c
ehdtjr19960409 <ehdtjr19960409@gmail.com>
2024-11-25 09:18:40 +0900

    241125 1주차 주말과제

---
+++ week01/src/homework/PrintInfo.java
@@ -0,0 +1,8 @@
+package homework; //homework 이라는 package 소스파일들의 묶음
+
+public class PrintInfo {  //클래스 PrintInfo 생성 , {=클래스 시작
+	public static void main(String[] args) { //main 메소드이고 자바 프로그램 실행 시 메인 메소드가 먼저 실행
+											// {=main 메소드 시작
+		System.out.println("신동석"+29); //츌력 메소드를 활용한 "값" 을 출력
+	}	//}= main 메소드 닫힘
+} //}= class 닫힘
---
+++ week01/src/homework/PrintInfo2.java
@@ -0,0 +1,9 @@
+package homework; //homework 이라는 package 소스파일들의 묶음
+
+public class PrintInfo2 { //클래스 PrintInfo2 생성 , {=클래스 시작
+	public static void main(String[] args) { //main 메소드이고 자바 프로그램 실행 시 메인 메소드가 먼저 실행
+											// {=main 메소드 시작	
+		System.out.print("2024년 11월 "); // 츌력 메소드를 활용한 "값" 을 출력
+		System.out.println("24일 02시 00분"); //츌력 메소드를 활용한 "값" 을 출력
+	}	//}= main 메소드 닫힘
+} //}= class 닫힘
